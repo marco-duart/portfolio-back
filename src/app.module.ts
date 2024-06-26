485dd9df7e4977f0601e2395682fb8a438ec3e8e
Marco Aurélio <marcoaurelio_db@hotmail.com>
2024-06-26 11:33:14 -0300

    fix: fix dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { EducationsModule } from './educations/educations.module';
 import { ExperiencesModule } from './experiences/experiences.module';
@@ -7,10 +9,11 @@ import { ResumesModule } from './resumes/resumes.module';
 import { SkillsModule } from './skills/skills.module';
 import { PortfolioItemsModule } from './portfolio-items/portfolio-items.module';
 
-
 @Module({
   imports: [
+    DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
     UsersModule,
     EducationsModule,
     ExperiencesModule,
